f0447f1b7569c75deef9bc9814e5dafa6e541bf7
Niewtone <victor.sorais@epitech.eu>
2021-09-14 09:41:03 +0200

    init model

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</header>
+<body>
+    <script src="motion.js"></script>
+    <head>
+
+    </head>
+            <div class="main">
+                <div class="main-left-container">
+                    <div class="dash-title-container">
+                        <span class="dash-title">dr-healer</span>
+                    </div>
+                    <div class="dash-part1-title-container">
+                        <span class="dash-part1-title">dashboard</span>
+                    </div>
+                    <div class="dash-subtitle-container"><span class="dash-part1-subtitle">apps</span></div>
+                    <div class="dash-menu-container">   
+                        <div class="UI-box-menu">
+                            Home
+                        </div>
+                        <div class="UI-box-menu">
+                            Your Drone
+                        </div>
+                        <div class="UI-box-menu">
+                            Crops
+                        </div>
+                        <div class="UI-box-menu">
+                            Widget
+                        </div>
+                        <div class="UI-box-menu">
+                            Message
+                        </div>
+                        <div class="UI-box-menu">
+                            Advising
+                        </div>
+                        <div class="UI-box-menu">
+                            shop
+                        </div>
+                        <div class="UI-box-menu">
+                            Profil
+                        </div>
+                    </div>
+                    <div class="dash-subtitle-container"><span class="dash-part1-subtitle">Support</span></div>
+                    <div class="dash-menu-container">   
+                        <div class="UI-box-menu">
+                            contact us
+                        </div>
+                        <div class="UI-box-menu">
+                            Advising
+                        </div>
+                        <div class="UI-box-menu">
+                            Help
+                        </div>
+                    </div>
+
+                </div>
+                <div class="main-right-container">
+                    <div class="container-fluid">
+                        <div class="row mynav">
+                            <div class="col-1">
+                                <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="50" height="50">
+                            </div>
+                            <div class="col-3">
+
+                            </div>
+                            <div class="col-4">
+
+                            </div>
+                            <div class="col-2">
+
+                            </div>
+                            <div class="col-2">
+                                <div class="nav-btn-3">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <div class="row dash-home-container">
+                            <div class="col-lg-5 dash-h-c">
+                                <div class="UI-home-dash-box">
+
+                                </div>
+                            </div>
+                            <div class="col-lg-7 dash-h-c">
+                                <div class="UI-home-dash-box">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row dash-home-container">
+                            <div class="col-lg-12 dash-h-c2">
+                                <div class="UI-home-dash-box">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row dash-home-container">
+                            <div class="col-lg-7 dash-h-c">
+                                <div class="UI-home-dash-box">
+
+                                </div>
+                            </div>
+                            <div class="col-lg-5 dash-h-c">
+                                <div class="UI-home-dash-box">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</body>
+</html>
